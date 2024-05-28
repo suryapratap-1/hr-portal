5cb7d5b39f67ba1673d7819a04a725180e05fba3
surya pratap <suryapratap0765@gmail.com>
2024-05-28 12:26:47 +0530

    update page.tsx 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 
-export default function Home({ children, }: Readonly<{ children: React.ReactNode; }>) {
+export default function Home() {
     return (
         <main className="min-h-screen p-4 bg-gray-600 dark:bg-inherit dark:text-white">
             <h1 className="flex justify-center text-3xl">Home Page</h1>
-            {children}
         </main>
     );
 }
